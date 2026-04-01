1a3193ea42bff05016b68991928d201b1239dd5a
goesik123-raja <goesik123@gmail.com>
2026-04-01 12:41:10 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -1,21 +1,29 @@
+
+
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+
 const app = express();
 
-
+// ✅ CORS FIX (IMPORTANT)
 app.use(cors({
   origin: "https://gowsikdb-crud.vercel.app",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type"],
 }));
 
+app.options("*", cors()); // ✅ handle preflight
+
 app.use(express.json());
+
 
 const FoodModel = require("./models/food");
 
 // MongoDB connection
-mongoose.connect("mongodb+srv://admin:admin@cluster0.lsxuxwt.mongodb.net/?appName=Cluster0/food")
+// mongoose.connect("mongodb+srv://admin:admin@cluster0.lsxuxwt.mongodb.net/?appName=Cluster0/food")
+
+mongoose.connect("mongodb+srv://admin:admin@cluster0.lsxuxwt.mongodb.net/food?retryWrites=true&w=majority")
     .then(() => console.log("MongoDB Connected"))
     .catch(err => console.log(err));
 
@@ -91,6 +99,10 @@ app.delete("/delete/:id", async (req, res) => {
 
 
 //  SERVER
+
+const PORT = process.env.PORT || 3001;
+
+
 app.listen(3001, () => {
     console.log("Server running on port 3001");
 });
